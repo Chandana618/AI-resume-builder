92142a12e58735db8ccb02917c922fd23166846f
Chandana618 <chandanaadigoppula10@gmail.com>
2025-10-07 09:43:21 +0530

    added build to docs folder

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/AI-resume-builder/", // IMPORTANT: Add your repository name here for correct asset paths
+  build: {
+    outDir: 'docs' // Change the output folder from 'dist' to 'docs'
+  }
 })
